ffddc1ef39683a4041c9940b7f580c2511f81985
chornge <christianmbaba@live.com>
2016-12-22 10:58:09 -0600

    Dynamically adding search fragment to explore fragment.

--- app/src/main/java/com/chornge/nprime/SearchFragment.java
+++ app/src/main/java/com/chornge/nprime/SearchFragment.java
@@ -8,15 +8,35 @@ import android.view.ViewGroup;
 
 public class SearchFragment extends Fragment {
 
+    //mandatory
     public SearchFragment() {
-        // Required empty public constructor
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_search, container, false);
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+    }
 }

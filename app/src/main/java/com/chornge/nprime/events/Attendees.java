f563546d98801c6163e810334615676b4929b704
chornge <christianmbaba@live.com>
2017-02-06 23:07:34 -0600

    Refactoring.

--- app/src/main/java/com/chornge/nprime/events/Attendees.java
+++ app/src/main/java/com/chornge/nprime/events/Attendees.java
@@ -2,12 +2,8 @@ package com.chornge.nprime.events;
 
 import com.chornge.nprime.users.User;
 
-import java.util.List;
-
 interface Attendees {
     void addGuest(User user);
-
-    List<User> getGuests();
 
     void removeGuest(User user);
 }
--- app/src/main/java/com/chornge/nprime/events/Event.java
+++ app/src/main/java/com/chornge/nprime/events/Event.java
@@ -99,7 +99,6 @@ public class Event implements Attendees {
         guests.remove(guestToRemove);
     }
 
-    @Override
     public List<User> getGuests() {
         return guests;
     }

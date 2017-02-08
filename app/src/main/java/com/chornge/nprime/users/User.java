65e1d8b045568e5f1be1ff8b6c91afdfa554d984
chornge <christianmbaba@live.com>
2017-02-08 09:24:38 -0600

    Created use case template.

--- app/src/main/java/com/chornge/nprime/users/User.java
+++ app/src/main/java/com/chornge/nprime/users/User.java
@@ -2,31 +2,21 @@ package com.chornge.nprime.users;
 
 import android.widget.ImageButton;
 
-import com.chornge.nprime.events.Event;
-
-import java.util.List;
-
-public class User implements Networking {
+public class User { //implements Networking {
     private String userName;
-    private String userID;
     private ImageButton userPhoto;
     private String userEmail;
-    private List<Event> userEventList;
-    private List<User> followers;
-    private List<User> peopleUserFollows;
+//    private List<Event> userEventList;
+//    private List<User> followers;
+//    private List<User> peopleUserFollows;
 
     public User() {
-        //blank default constructor required for firebase
+        //blank default constructor
     }
 
-    public User(String userID, String userEmail) {
-        this.userName = "";
-        this.userID = userID;
+    public User(String userName, String userEmail) {
+        this.userName = userName;
         this.userEmail = userEmail;
-    }
-
-    public String getUserID() {
-        return userID;
     }
 
     public ImageButton getUserPhoto() {
@@ -37,7 +27,9 @@ public class User implements Networking {
         this.userPhoto = newUserPhoto;
     }
 
-    // ...look into ThreadLocal...
+    /**
+     * look into ThreadLocal
+     */
 
     public String getUserEmail() {
         return userEmail;
@@ -55,31 +47,31 @@ public class User implements Networking {
         this.userName = newUserName;
     }
 
-    @Override
-    public void addFollower(User user) {
-        followers.add(user);
-    }
+//    @Override
+//    public void addFollower(User user) {
+//        followers.add(user);
+//    }
+//
+//    @Override
+//    public void removeFollower(User user) {
+//        followers.remove(user);
+//    }
+//
+//    @Override
+//    public void startFollowing(User user) {
+//        peopleUserFollows.add(user);
+//    }
+//
+//    @Override
+//    public void stopFollowing(User user) {
+//        peopleUserFollows.remove(user);
+//    }
 
-    @Override
-    public void removeFollower(User user) {
-        followers.remove(user);
-    }
-
-    @Override
-    public void startFollowing(User user) {
-        peopleUserFollows.add(user);
-    }
-
-    @Override
-    public void stopFollowing(User user) {
-        peopleUserFollows.remove(user);
-    }
-
-    public List<User> getFollowers() {
-        return followers;
-    }
-
-    public List<User> getPeopleUserFollows() {
-        return peopleUserFollows;
-    }
+//    public List<User> getFollowers() {
+//        return followers;
+//    }
+//
+//    public List<User> getPeopleUserFollows() {
+//        return peopleUserFollows;
+//    }
 }
